c0f76d0cb4b85472b158cc796706c432010b7d03
Admin <dance.with.me@bk.ru>
2025-09-27 13:01:23 +0700

    end project

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -6,20 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.modifierdemo.ui.theme.ModifierDemoTheme
+import androidx.compose.ui.draw.clip
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,16 +46,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
+
     val mymodifier = modifier
-        .padding(all = 10.dp)
         .border(width = 2.dp, color = Color.Magenta)
-    Text(
-        text = "Hello Compose!",
-        mymodifier,
-        fontSize = 40.sp,
-        fontWeight = FontWeight.Bold
-    )
+        .padding(all = 10.dp)
+
+    val secondModifier = Modifier.height(100.dp)
+
+    Column(
+        Modifier.padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            "Hello Compose!",
+            mymodifier.then(secondModifier),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+            )
+            Spacer(Modifier.height(16.dp))
+            CustomImage(R.drawable.vacation,
+                Modifier
+                    .padding(16.dp)
+                    .width(270.dp)
+                    .clip(shape = RoundedCornerShape(30.dp))
+            )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
